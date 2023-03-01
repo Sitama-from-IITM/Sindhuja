b2113686f80ef9fe953b111b8525e5a3291e99e4
Carnage_007 <95233345+SheikhAbdulNishar@users.noreply.github.com>
2023-03-01 16:26:05 +0530

    navbar added

--- script.js
+++ script.js
@@ -19,3 +19,39 @@ function myFunction() {
 //     document.querySelector("body").classList.add("block");
 //   })
 // }
+
+
+/* Script for navigation bar */
+
+const dropDownButtonEl = document.getElementById('drop-down-button');
+const dropDownContentEl = document.querySelector('.drop-down-content');
+const navBarLinksEl = document.querySelectorAll('.nav-item');
+
+/* Adding Functionality to Contact Us Button */
+dropDownButtonEl.addEventListener('click', function(){
+    if(dropDownContentEl.style.display == 'flex')
+      dropDownContentEl.style.setProperty('display', 'none');
+    else 
+      dropDownContentEl.style.setProperty('display', 'flex');
+  });
+  
+  /* Removing Dropdown Content if any Nav Bar Button other than Contact Us is Clicked */
+  for(var i = 0; i < navBarLinksEl.length; i++){
+  navBarLinksEl[i].addEventListener('click', function(){
+    dropDownContentEl.style.setProperty('display', 'none');
+  });
+  }
+
+document.querySelector('.icon').addEventListener('click', function(){
+  {
+    document.querySelector('.nav-bar').style.setProperty('display', 'block');
+    var x = document.querySelectorAll(".left-container,.right-container");
+      for(var i = 0; i < x.length; i++){
+      if (x[i].style.display === "block") {
+        x[i].style.display = "none";
+      } else {
+        x[i].style.display = "block";
+      }
+    }
+  }
+});
